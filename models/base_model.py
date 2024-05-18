481ff59aacf0a1b5e7441544e13dc921800d3840
lukatada <lukaharuna4@gmail,com>
2024-05-18 08:23:17 -0700

    Model

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,28 @@
+import uuid
+from datetime import datetime
+
 class BaseModel():
-    def __init__(self, uuid, created_at, updated_at):
-        self.id = uuid
-        self.created_at = created_at
-        self.updated_at = updated_at
+
+    """public instance attributes"""
+    id = None
+    created_at = None
+    updated_at = None
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+    
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
 
     def save(self):
+        self.update_at = datetime.now()
+
     def to_dict(self):
-    def __str__:
+        return {
+                "__class__": self.__class__.__name__,
+                "id": self.id,
+                "created_at": self.created_at.isoformat(),
+                "updated_at": self.updated_at.isoformat()
+                }
