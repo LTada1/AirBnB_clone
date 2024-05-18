39dc13047d68300bf2950ee69e90856163978bd1
lukatada <lukaharuna4@gmail,com>
2024-05-18 08:36:23 -0700

    models

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,8 @@
 import uuid
 from datetime import datetime
 
-class BaseModel():
 
+class BaseModel():
     """public instance attributes"""
     id = None
     created_at = None
@@ -12,7 +12,7 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-    
+
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
 
