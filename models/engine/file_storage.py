2554021def35bb53d83eeacfe6cd3880d9e715ec
lukatada <lukaharuna4@gmail,com>
2024-05-21 03:41:06 -0700

    instance

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,32 +13,31 @@ import json
 import BaseModel
 
 class FileStorage:
+    "Private class attributes"
     __file_path
     __objects = {}
 
-    def __init__(self):
-        self.__file_path = __file_path
-        self.__objects = __objects
     def all(self):
         "returns the dictionary __objects"
-        return BaseModel.__objects
+        return FileStorage.__objects
     def new(self, obj):
         "sets in __objects the obj with key <obj class name>.id"
         key = f"{obj.__class__.__name__.{obj.id}"
-        BaseModel.__objects[key] = obj
+        FileStorage.__objects[key] = obj
     def save(self):
         "serializes __objects to the JSON file"
-        with open("BaseModel.__file_path", "w") as file_json:
-            json_objects = {key:obj.__dict__ for key, obj in BaseModel.__objects.items()}
+        with open('FileStorage.__file_path', 'w') as file_json:
+            json_objects = {key:obj.__dict__ for key, obj in FileStorage.__objects.items()}
         json.dumps(json_file, file_json)
     def reload(self):
         "deserializes the JSON file to __objects"
-            try:
-                with open('BaseModel.__file_patth', 'r') as json_file:
-                    json_objects = json.loads(json_file)
-                    for key, value in json_objects.items():
-                        obj = BaseModel(**values)
-                        BaseModel.__objects[key] = obj
-            except FileNotFoundError:
+        try:
+            with open('FileStorage.__file_patth', 'r') as json_file:
+                json_objects = json.loads(json_file)
+                for key, value in json_objects.items():
+                    "BaseModel is imported"
+                    obj = BaseModel(**values)
+                    BaseModel.__objects[key] = obj
+        except FileNotFoundError:
             pass
 
